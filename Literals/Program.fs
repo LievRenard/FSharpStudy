9e5daa42ec982a2f585a50b3148e29e8c8eb2c71
LievRenard <emevanpoivel@gmail.com>
2023-02-23 13:56:16 +0900

    update literals

--- Literals/Program.fs
+++ Literals/Program.fs
@@ -3,6 +3,15 @@ printfn "Hello from F#"
 
 let a = 1
 let b = 2.0
+let c = 10f
 
-printfn "%d" a
+printfn "%A" a
 printfn $"{b}"
+printfn "%A" c
+
+let type1 = c.GetType()
+printfn "%A" type1
+
+let numbers = (0x9F, 0o77, 0b1010)
+printfn "%A" numbers
+numbers.GetType() |> printfn "%A"
